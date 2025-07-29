bcf8e8d27305c9fb167a799455cec6ca59810288
liuzihao <1325083198@qq.com>
2025-07-29 16:55:30 +0800

    update sdk commit

--- sdk_kws_customer/script/git_hash.h
+++
@@ -1,7 +0,0 @@
-// This file is auto-generated. Do not edit manually.
-#ifndef GIT_HASH_H
-#define GIT_HASH_H
-
-#define GIT_COMMIT_HASH "578e7026b8bba91df9dfee499c24248927190872"
-
-#endif // GIT_HASH_H
