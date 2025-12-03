bf76abc683c403f64f22ba550bd21fbefadb82aa
liuzihao <1325083198@qq.com>
2025-12-03 17:54:20 +0800

    update soc version

--- soc/application/inc/soc_version.h
+++ soc/application/inc/soc_version.h
@@ -1,8 +1,8 @@
 #include "git_hash.h"
 
-#define SOC_VERSION_MAJOR 1
-#define SOC_VERSION_MINOR 2
-#define SOC_VERSION_PATCH 9
+#define SOC_VERSION_MAJOR 2
+#define SOC_VERSION_MINOR 0
+#define SOC_VERSION_PATCH 0
 
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
