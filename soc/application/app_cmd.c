d920e8f9e535b88b870173a4f1ab598b8afb0770
liuzihao <1325083198@qq.com>
2026-05-29 14:32:21 +0800

    support print lib version

--- soc/application/app_cmd.c
+++ soc/application/app_cmd.c
@@ -132,6 +132,7 @@ void app_cmd_reg_write_running(const char *cmd) {
 void app_cmd_get_version(const char *cmd) {
     DTOF_LOG("soc version: %s\n", SOC_VERSION_STRING);
     DTOF_LOG("sdk version: %s\n", dtof_get_sdk_version());
+    DTOF_LOG("lib version: %s\n", dtof_get_lib_version());
     DTOF_LOG("ram version: %d\n", DTOF_SWAP16(dtof_get_chip_version()));
     DTOF_LOG("chip id: 0x%04x\n", dtof_get_chip_config()->chip_id);
     DTOF_LOG("chip uuid: ");
